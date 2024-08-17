c318c620ed8496825a28c86fcfb98a858fbc460f
harish3737 <37335142+harish3737@users.noreply.github.com>
2024-08-17 15:25:23 +0530

    Binded View Model and optimised code

--- ColorWheelApp/View/VC/ColorWheelViewController.swift
+++ ColorWheelApp/View/VC/ColorWheelViewController.swift
@@ -18,6 +18,7 @@ class ColorWheelViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setupTableView()
+        bindViewModel()
     }
 
 
@@ -29,53 +30,34 @@ extension ColorWheelViewController {
     
     private func setupTableView() {
         tableView.delegate = self
-              tableView.dataSource = self
+               tableView.dataSource = self
 
-              // Register the custom cells with XIBs
-        tableView.register(UINib(nibName: "SegmentedControlCollectionViewCell", bundle: nil), forCellReuseIdentifier: SegmentedControlCollectionViewCell.identifier)
-        tableView.register(UINib(nibName: "BrightnessSliderTableViewCell", bundle: nil), forCellReuseIdentifier: BrightnessSliderTableViewCell.identifier)
-        tableView.register(ColorWheelTableViewCell.self, forCellReuseIdentifier: ColorWheelTableViewCell.identifier)
-        
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = 100
-        tableView.separatorStyle = .none  // Remove cell separators for a cleaner look
-        tableView.backgroundColor = .black  // Match the background color to the color wheel
-        
-        // Observers for handling segment and brightness changes
-                NotificationCenter.default.addObserver(self, selector: #selector(didSelectSegment(_:)), name: .didSelectSegment, object: nil)
-                NotificationCenter.default.addObserver(self, selector: #selector(didUpdateBrightness), name: .didUpdateBrightness, object: nil)
-            NotificationCenter.default.addObserver(self, selector: #selector(didSelectColor(_:)), name: .colorWheelDidSelectColor, object: nil)
+               // Register the custom cells with XIBs
+               tableView.register(UINib(nibName: "SegmentedControlCollectionViewCell", bundle: nil), forCellReuseIdentifier: SegmentedControlCollectionViewCell.identifier)
+               tableView.register(UINib(nibName: "BrightnessSliderTableViewCell", bundle: nil), forCellReuseIdentifier: BrightnessSliderTableViewCell.identifier)
+               tableView.register(ColorWheelTableViewCell.self, forCellReuseIdentifier: ColorWheelTableViewCell.identifier)
+               
+               tableView.rowHeight = UITableView.automaticDimension
+               tableView.estimatedRowHeight = 100
+               tableView.separatorStyle = .none  // Remove cell separators for a cleaner look
+               tableView.backgroundColor = .black  // Match the background color to the color wheel
             
             
             
     }
     
-    @objc private func didSelectSegment(_ notification: Notification) {
-           if let index = notification.userInfo?["index"] as? Int {
-               viewModel.selectedSegmentIndex = index
-               tableView.reloadData() // Reload the table view to update the color wheel and slider
-           }
-       }
-    
-    @objc private func didSelectColor(_ notification: Notification) {
-            if let color = notification.userInfo?["color"] as? UIColor {
-                     var hue: CGFloat = 0
-                       var saturation: CGFloat = 0
-                       var brightness: CGFloat = 0
-                       var alpha: CGFloat = 0
-                color.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
-                      
-                viewModel.colors[viewModel.selectedSegmentIndex] =  ColorModel(hue: hue, saturation: saturation, brightness: brightness)
-                tableView.reloadData()  // Update the table view with the new color
+    private func bindViewModel() {
+            // Bind the view model's closure callbacks to update the UI when the model changes
+            viewModel.onSegmentChange = { [weak self] in
+                self?.tableView.reloadData()
+            }
+            viewModel.onColorChange = { [weak self] in
+                self?.tableView.reloadData()
+            }
+            viewModel.onBrightnessChange = { [weak self] in
+                self?.tableView.reloadData()
             }
         }
-       
-    @objc private func didUpdateBrightness(_ notification: Notification) {
-        if let brightness = notification.userInfo?["brightness"] as? CGFloat {
-            viewModel.colors[viewModel.selectedSegmentIndex].updateBrightness(to: brightness)
-            tableView.reloadData()  // Update the table view to reflect the new brightness
-        }
-    }
     
     
 }
@@ -87,24 +69,25 @@ extension ColorWheelViewController: UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.row == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: SegmentedControlCollectionViewCell.identifier, for: indexPath) as! SegmentedControlCollectionViewCell
-                cell.configure(with: viewModel.colors.map { $0.color }, selectedIndex: viewModel.selectedSegmentIndex)
-            cell.backgroundColor = .clear  // Transparent background to blend with overall design
-            return cell
-        } else if indexPath.row == 1 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: ColorWheelTableViewCell.identifier, for: indexPath) as! ColorWheelTableViewCell
-            cell.configure(with: viewModel, selectedIndex: viewModel.selectedSegmentIndex)
-                      return cell
-        } else if indexPath.row == 2 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: BrightnessSliderTableViewCell.identifier, for: indexPath) as! BrightnessSliderTableViewCell
-            cell.configureSlider(for: viewModel.selectedSegmentIndex, with: viewModel)
-            cell.backgroundColor = .clear  // Transparent background
-            return cell
-        }
+          if indexPath.row == 0 {
+              let cell = tableView.dequeueReusableCell(withIdentifier: SegmentedControlCollectionViewCell.identifier, for: indexPath) as! SegmentedControlCollectionViewCell
+              cell.configure(with: viewModel.colors.map { $0.color }, selectedIndex: viewModel.selectedSegmentIndex)
+              cell.backgroundColor = .clear  // Transparent background to blend with overall design
+              return cell
+          } else if indexPath.row == 1 {
+              let cell = tableView.dequeueReusableCell(withIdentifier: ColorWheelTableViewCell.identifier, for: indexPath) as! ColorWheelTableViewCell
+              cell.configure(with: viewModel, selectedIndex: viewModel.selectedSegmentIndex)
+              return cell
+          } else if indexPath.row == 2 {
+              let cell = tableView.dequeueReusableCell(withIdentifier: BrightnessSliderTableViewCell.identifier, for: indexPath) as! BrightnessSliderTableViewCell
+              cell.configureSlider(for: viewModel.selectedSegmentIndex, with: viewModel)
+              cell.backgroundColor = .clear  // Transparent background
+              return cell
+          }
 
-        return UITableViewCell()
-    }
+          return UITableViewCell()
+      }
+
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         let screenHeight = UIScreen.main.bounds.height
--- ColorWheelApp/ViewModel/ColorViewModel.swift
+++ ColorWheelApp/ViewModel/ColorViewModel.swift
@@ -8,7 +8,6 @@
 import Foundation
 import UIKit
 
-
 class ColorViewModel {
     var colors: [ColorModel] = [
         ColorModel(hue: 0.48, saturation: 0.78, brightness: 1.0),
@@ -16,9 +15,53 @@ class ColorViewModel {
         ColorModel(hue: 0.08, saturation: 1.0, brightness: 1.0)
     ]
 
-    var selectedSegmentIndex: Int = 0 // Tracks the selected segment index
+    var selectedSegmentIndex: Int = 0 {
+        didSet {
+            onSegmentChange?()
+        }
+    }
 
+    // Closures for binding to the view controller
+    var onSegmentChange: (() -> Void)?
+    var onColorChange: (() -> Void)?
+    var onBrightnessChange: (() -> Void)?
 
+    init() {
+        setupObservers()
+    }
+
+    // Setup NotificationCenter observers in the ViewModel
+    private func setupObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(didSelectSegment(_:)), name: .didSelectSegment, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(didUpdateBrightness(_:)), name: .didUpdateBrightness, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(didSelectColor(_:)), name: .colorWheelDidSelectColor, object: nil)
+    }
+
+    @objc private func didSelectSegment(_ notification: Notification) {
+        if let index = notification.userInfo?["index"] as? Int {
+            selectedSegmentIndex = index
+        }
+    }
+
+    @objc private func didSelectColor(_ notification: Notification) {
+        if let color = notification.userInfo?["color"] as? UIColor {
+            var hue: CGFloat = 0
+            var saturation: CGFloat = 0
+            var brightness: CGFloat = 0
+            var alpha: CGFloat = 0
+            color.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
+
+            colors[selectedSegmentIndex] = ColorModel(hue: hue, saturation: saturation, brightness: brightness)
+            onColorChange?() // Notify the view controller of the color change
+        }
+    }
+
+    @objc private func didUpdateBrightness(_ notification: Notification) {
+        if let brightness = notification.userInfo?["brightness"] as? CGFloat {
+            colors[selectedSegmentIndex].updateBrightness(to: brightness)
+            onBrightnessChange?() // Notify the view controller of the brightness change
+        }
+    }
 
     func color(at index: Int) -> UIColor {
         return colors[index].color
@@ -27,7 +70,6 @@ class ColorViewModel {
     func brightness(at index: Int) -> CGFloat {
         return colors[index].brightness
     }
-
-
 }
+
 
